e1162d42a2c1e3e28411a5febaf9fd705713ef3c
apollozeus042 <0042apollozeus@gmail.com>
2023-10-27 00:51:46 +0800

    Added few visual changes

--- lib/pages/menu_page.dart
+++ lib/pages/menu_page.dart
@@ -1,3 +1,4 @@
+import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:paytriot/pages/create_acc_page.dart';
 import 'package:paytriot/pages/read_id_page.dart';
@@ -15,25 +16,34 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Color(0xFF007229)),
         useMaterial3: true,
       ),
       home: const MenuPage(title: 'Flutter Demo Home Page'),
     );
   }
 }
+class ButtonStyleNotifier extends ChangeNotifier {
+  bool isCreateAccButtonPressed = false;
 
+  void toggleCreateAccButton() {
+    isCreateAccButtonPressed = !isCreateAccButtonPressed;
+    notifyListeners();
+  }
+}
 class MenuPage extends StatefulWidget {
   const MenuPage({Key? key, required this.title});
 
   final String title;
 
   @override
+
   State<MenuPage> createState() => _MenuPageState();
 }
 
 class _MenuPageState extends State<MenuPage> {
   final box = GetStorage();
+  bool isCreateAccButtonPressed = false;
 
   void getAction(String action) async {
     await GetStorage.init();
@@ -42,7 +52,7 @@ class _MenuPageState extends State<MenuPage> {
         context, MaterialPageRoute(builder: (context) => ReadIdPage()));
   }
 
-  Widget buttToCreateAcc() => TextButton(
+  Widget buttToCreateAcc() => OutlinedButton(
         onPressed: () {
           Navigator.push(context,
               MaterialPageRoute(builder: (context) => CreateAccPage()));
@@ -50,23 +60,41 @@ class _MenuPageState extends State<MenuPage> {
         child: const Text(
           'Create Account',
         ),
+        style: OutlinedButton.styleFrom(
+          backgroundColor:
+                Colors.white,
+          foregroundColor:
+                Color(0xFF007229),
+        ),
       );
 
-  Widget buttToCashIn() => TextButton(
+  Widget buttToCashIn() => OutlinedButton(
         onPressed: () {
           getAction('CashIn');
         },
         child: const Text(
           'Cash In',
         ),
+        style: OutlinedButton.styleFrom(
+          backgroundColor:
+              Colors.white,
+          foregroundColor:
+              Color(0xFF007229),
+        ),
       );
 
-  Widget buttToPurchase() => TextButton(
+  Widget buttToPurchase() => OutlinedButton(
         onPressed: () {
           getAction('Purchase');
         },
         child: const Text(
           'Purchase',
+        ),
+        style: OutlinedButton.styleFrom(
+          backgroundColor:
+              Colors.white,
+          foregroundColor:
+              Color(0xFF007229),
         ),
       );
 

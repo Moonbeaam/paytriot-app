5a7ad9cd224ec379fba2e798f6dbd9e47eed7a3f
Moonbeaam <therandomanything@gmail.com>
2023-10-26 23:22:35 +0800

    added pop-up notification and return to home page

--- lib/pages/cash_in_page.dart
+++ lib/pages/cash_in_page.dart
@@ -3,42 +3,43 @@ import 'package:paytriot/DB/stud_acc_db.dart';
 import 'package:paytriot/model/stud_acc.dart';
 import 'package:get_storage/get_storage.dart';
 
-
 class CashInPage extends StatefulWidget {
   @override
-
   State<CashInPage> createState() => _CashInPageState();
 }
 
 class _CashInPageState extends State<CashInPage> {
-
   String studNum = '';
-  String displayText='';
-  String cashInAmount='';
+  String displayText = '';
+  String cashInAmount = '';
   late StudAcc studentAccount;
   final box = GetStorage();
 
   void readStudAcc() async {
     try {
-      final result = await StudAccDB.instance.readAcc(box.read('studNum')); // Use your readAcc function
+      final result = await StudAccDB.instance
+          .readAcc(box.read('studNum')); // Use your readAcc function
       setState(() {
         studentAccount = result;
-        displayText = "Name:${studentAccount.firstName} ${studentAccount.middleName} "
+        displayText =
+            "Name: ${studentAccount.firstName} ${studentAccount.middleName} "
             "${studentAccount.lastName}\n"
             "Student Number: ${studentAccount.studNum}\n"
             "Balance: ${studentAccount.balance}";
       });
     } catch (e) {
-      displayText= "not found";
+      displayText = "not found";
     }
   }
+
   void cashIn() async {
     int newBalance = studentAccount.balance + (int.tryParse(cashInAmount) ?? 0);
     StudAccDB.instance.updateBalance(newBalance, studentAccount.studNum);
     studentAccount = studentAccount.copy(balance: newBalance);
 
     setState(() {
-      displayText = "Name: ${studentAccount.firstName} ${studentAccount.middleName} "
+      displayText =
+          "Name: ${studentAccount.firstName} ${studentAccount.middleName} "
           "${studentAccount.lastName}\n"
           "Student Number: ${studentAccount.studNum}\n"
           "Balance: ${studentAccount.balance}";
@@ -46,19 +47,19 @@ class _CashInPageState extends State<CashInPage> {
   }
 
   Widget buildCashIn() => TextField(
-    decoration: const InputDecoration(
-      labelText: 'Cash In Amount',
-      border: OutlineInputBorder(),
-    ),
-    onChanged: (value) => setState(() => cashInAmount = value),
-  );
+        decoration: const InputDecoration(
+          labelText: 'Cash In Amount',
+          border: OutlineInputBorder(),
+        ),
+        onChanged: (value) => setState(() => cashInAmount = value),
+      );
 
   Widget btnCashIn() => TextButton(
-    onPressed: cashIn,
-    child: const Text(
-      'Cash In',
-    ),
-  );
+        onPressed: cashIn,
+        child: const Text(
+          'Cash In',
+        ),
+      );
 
   @override
   void initState() {
@@ -74,9 +75,7 @@ class _CashInPageState extends State<CashInPage> {
       ),
       body: ListView(
         children: [
-          Text(
-              "$displayText"
-          ),
+          Text("$displayText"),
           buildCashIn(),
           const SizedBox(height: 16, width: 5),
           btnCashIn()

c03c8c5d3acec10e41b8313ad6acfcafa96c9337
apollozeus042 <0042apollozeus@gmail.com>
2023-11-28 15:03:26 +0800

    fixed AES keys and env files

--- lib/pages/main.dart
+++ lib/pages/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:paytriot/pages/home_page.dart';
 
-void main() {
+Future<void> main() async {
+  await dotenv.load();
+
   runApp(const MyApp());
 }
 
